67484cd311b15d3212b02df5b6dafb0489e71527
xArkqngel <arkqngel343@gmail.com>
2022-03-19 23:14:18 -0500

    upload

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const{src,dest,watch} = require('gulp');
+const sass= require('gulp-sass')(require('sass'));
+const plumber= require('gulp-plumber');
+
+function css(done){
+    src('src/scss/**/*.scss')//ubica
+        .pipe(plumber())
+        .pipe(sass())//compila
+        .pipe(dest('build/css'))//guarda
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss',css);
+    done();
+}
+
+exports.css=css;
+exports.dev=dev;
