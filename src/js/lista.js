57e45361260fcc330ff0c6bf79d3a249c8811a0d
xArkqngel <arkqngel343@gmail.com>
2022-03-20 22:10:56 -0500

    stylizing carrito

---
+++ src/js/lista.js
@@ -0,0 +1,16 @@
+function User(name, lastname, city, email) {
+  this.name = name;
+  this.lastname = lastname;
+  this.city = city;
+  this.email = email;
+}
+
+let myArr = [
+  new User('Leonardo', 'Parigini', 'Santorini', 'leoparigini@gmail.com'),
+  new User('Edoardo', 'Merlini', 'Monaco', 'edomerlini@gmail.com'),
+  new User('Nicole', 'De Lellis', 'Milano', 'nickydelellis@gmail.com'),
+  new User('Jasmine', 'Scherzinger', 'Los Angeles', 'jazzylax@gmail.com'),
+  new User('Emily', 'Knowles', 'San Francisco', 'emilyknow@gmail.com')
+]
+
+document.querySelector("#myTable tbody").innerHTML = myArr.map(user => `<tr><td>${user.name}</td><td>${user.lastname}</td><td>${user.city}</td><td>${user.email}</td></tr>`).join('')
